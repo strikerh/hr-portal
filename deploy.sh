44b643786670e3990d563313c836436d8ab94def
Hazem Safwat <h.nabil@hawsabah.sa>
2024-10-09 00:54:04 +0300

    init

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 cd /var/www/hr-portal
-echo "Pull the latest changes from the git repository"
+echo " --- Pull the latest changes from the git repository"
 git stash
 git pull
 
 
-echo  "Remove the old dist folder if it exists"
+echo  " --- Remove the old dist folder if it exists"
 if [ -d "./dist" ]; then
     rm -rf ./dist
 fi
 
-echo "Extract the dist.tar.gz file"
+echo " --- Extract the dist.tar.gz file"
 tar -xzvf dist.tar.gz -C ./
 
 sudo chmod +x deploy.sh
 
-echo "Deployed successfully"
+echo " --- Deployed successfully"
 echo "cd /var/www/hr-portal"
 echo "cd /var/www/hr-portal/dist/fuse/browser/"

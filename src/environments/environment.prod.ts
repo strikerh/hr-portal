c56a4740229e15f65daa00ca5ae2fba961c41e16
Hazem Safwat <h.nabil@hawsabah.sa>
2024-10-23 17:54:33 +0300

    'deploy'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'https://sahabah-new.hawsabah.sa',
     // Add other production-specific variables here
-    version: '1.1.27',
+    version: '1.1.28',
 };

fadba2b3cd230877f95f2256ebe1f844aef5c833
Ezz Aldien Dawoud <E.DAWOUD@hawsabah.sa>
2025-02-06 16:21:21 +0300

    test

---
+++ src/environments/environment.stg.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    // apiUrl: 'https://sahabah-new.hawsabah.sa',
+    apiUrl: 'http://50.116.48.108:8069',
+    // Add other development-specific variables here
+    version: '1.1.35',
+};

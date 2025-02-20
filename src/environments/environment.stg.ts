9f9aa25f9d4342c72184ae2c2f25cc823fe2a283
Ezz Aldien Dawoud <E.DAWOUD@hawsabah.sa>
2025-02-20 13:53:39 +0300

    'deploy-stg'

--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -3,5 +3,5 @@ export const environment = {
     // apiUrl: 'https://sahabah-new.hawsabah.sa',
     apiUrl: '//50.116.48.108:8069',
     // Add other development-specific variables here
-    version: 'stg : 1.1.38',
+    version: 'stg : 1.1.39',
 };
